3624e5965e3f0ad68a0e7dc58df39d93dede72cc
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:48:40 +0200

    2017.5

---
+++ src/main/java/solution/y2017/Day5_1.java
@@ -0,0 +1,37 @@
+package solution.y2017;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import utils.soution.MapSolution;
+
+public class Day5_1 extends MapSolution<List<Long>> {
+    public static void main(String[] args) {
+        new Day5_1().solve();
+    }
+
+    @Override
+    protected List<Long> initializeMapping() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    protected void transformInstruction(String instruction, List<Long> longs) {
+        longs.add(Long.parseLong(instruction));
+    }
+
+    @Override
+    protected String computeSolution(List<Long> longs) {
+        var index = 0;
+        var step = 0L;
+
+        while (index >= 0 && index < longs.size()) {
+            step++;
+            var offset = longs.get(index);
+            longs.set(index, offset + 1);
+            index += offset;
+        }
+
+        return step + "";
+    }
+}
