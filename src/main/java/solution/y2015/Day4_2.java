d59c4d9f96c7eb168c598c381f8ee48722c24142
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:47:40 +0200

    2015.4

---
+++ src/main/java/solution/y2015/Day4_2.java
@@ -0,0 +1,27 @@
+package solution.y2015;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import utils.Solution;
+
+public class Day4_2 extends Solution {
+	public static void main(String[] args) {
+		new Day4_2().solve();
+	}
+
+	@Override
+	protected String doSolve() {
+		String secretKey = input.getFirst();
+		long number = 1;
+
+		while (!doesHashHave5LeadingZeros(secretKey + number)) {
+			number++;
+		}
+
+		return String.valueOf(number);
+	}
+
+	private boolean doesHashHave5LeadingZeros(String input) {
+		var hash = DigestUtils.md5Hex(input);
+		return hash.startsWith("000000");
+	}
+}
