ec4d6a39a85d055d06fad4d11f966585a351aac9
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:50:58 +0200

    2021.20

---
+++ src/main/java/solution/y2021/Day20_2.java
@@ -0,0 +1,67 @@
+package solution.y2021;
+
+import utils.soution.Solution;
+
+public class Day20_2 extends Solution {
+	private final static int ROUNDS = 50;
+
+	public static void main(String[] args) {
+		new Day20_2().solve();
+	}
+
+	@Override
+	protected String doSolve() {
+		var algorithm = input.getFirst().split("");
+		var image = getImage();
+		for (var i = 0; i < ROUNDS; i++) {
+			var nextImage = new boolean[image.length][image[0].length];
+			for (var x = 0; x < nextImage.length; x++) {
+				for (var y = 0; y < nextImage[0].length; y++) {
+					var index = "";
+					index += getValue(image, x - 1, y - 1, i % 2 == 1) ? 1 : 0;
+					index += getValue(image, x, y - 1, i % 2 == 1) ? 1 : 0;
+					index += getValue(image, x + 1, y - 1, i % 2 == 1) ? 1 : 0;
+					index += getValue(image, x - 1, y, i % 2 == 1) ? 1 : 0;
+					index += getValue(image, x, y, i % 2 == 1) ? 1 : 0;
+					index += getValue(image, x + 1, y, i % 2 == 1) ? 1 : 0;
+					index += getValue(image, x - 1, y + 1, i % 2 == 1) ? 1 : 0;
+					index += getValue(image, x, y + 1, i % 2 == 1) ? 1 : 0;
+					index += getValue(image, x + 1, y + 1, i % 2 == 1) ? 1 : 0;
+					if (algorithm[Integer.parseInt(index, 2)].equals("#")) {
+						nextImage[x][y] = true;
+					}
+				}
+			}
+			image = nextImage;
+		}
+
+		var count = 0;
+		for (var booleans : image) {
+			for (var y = 0; y < image[0].length; y++) {
+				if (booleans[y]) {
+					count++;
+				}
+			}
+		}
+		return count + "";
+	}
+
+	private boolean getValue(boolean[][] image, int x, int y, boolean cornerValue) {
+		if (x < 0 || x >= image.length || y < 0 || y >= image[0].length) {
+			return cornerValue;
+		}
+		return image[x][y];
+	}
+
+	private boolean[][] getImage() {
+		var image = new boolean[input.get(2).length() + 2 + 2 * ROUNDS][input.size() + 2 * ROUNDS];
+		for (var y = 0; y < input.size() - 2; y++) {
+			for (var x = 0; x < input.get(y + 2).length(); x++) {
+				if (input.get(y + 2).charAt(x) == '#') {
+					image[x + ROUNDS + 1][y + ROUNDS + 1] = true;
+				}
+			}
+		}
+		return image;
+	}
+}
---
+++ src/test/java/solution/y2021/Day20_1Test.java
@@ -0,0 +1,14 @@
+package solution.y2021;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Disabled
+public class Day20_1Test {
+	@Test
+	public void test() {
+		assertThat(new Day20_1().disableLog().solve()).isEqualTo("5663");
+	}
+}
