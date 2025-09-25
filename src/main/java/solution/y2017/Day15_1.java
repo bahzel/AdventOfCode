60c08239a422e7709ef55b0af5a850cdaa1c2cc8
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:48:48 +0200

    2017.15

---
+++ src/main/java/solution/y2017/Day15_1.java
@@ -0,0 +1,46 @@
+package solution.y2017;
+
+import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import utils.soution.Solution;
+
+public class Day15_1 extends Solution {
+    public static void main(String[] args) {
+        new Day15_1().solve();
+    }
+
+    @Override
+    protected String doSolve() {
+        var count = 0;
+
+        var generatorA = new Generator(16807, Integer.parseInt(input.getFirst().split(" ")[4]));
+        var generatorB = new Generator(48271, Integer.parseInt(input.get(1).split(" ")[4]));
+
+        for (var i = 0L; i < 40000000; i++) {
+            generatorA.computeNext();
+            generatorB.computeNext();
+            if (generatorA.compare(generatorB)) {
+                count++;
+            }
+        }
+
+        return count + "";
+    }
+}
+
+@AllArgsConstructor
+class Generator {
+    protected final long factor;
+    protected long value;
+
+    public void computeNext() {
+        value = (value * factor) % 2147483647;
+    }
+
+    public boolean compare(Generator other) {
+        var binaryThis = StringUtils.leftPad(Long.toBinaryString(value), 16, '0');
+        var binaryOther = StringUtils.leftPad(Long.toBinaryString(other.value), 16, '0');
+
+        return binaryThis.substring(binaryThis.length() - 16).equals(binaryOther.substring(binaryOther.length() - 16));
+    }
+}
---
+++ src/main/java/solution/y2017/Day15_2.java
@@ -0,0 +1,42 @@
+package solution.y2017;
+
+import utils.soution.Solution;
+
+public class Day15_2 extends Solution {
+    public static void main(String[] args) {
+        new Day15_2().solve();
+    }
+
+    @Override
+    protected String doSolve() {
+        var count = 0;
+
+        var generatorA = new PickyGenerator(16807, Integer.parseInt(input.getFirst().split(" ")[4]), 4);
+        var generatorB = new PickyGenerator(48271, Integer.parseInt(input.get(1).split(" ")[4]), 8);
+
+        for (var i = 0L; i < 5000000; i++) {
+            generatorA.computeNext();
+            generatorB.computeNext();
+            if (generatorA.compare(generatorB)) {
+                count++;
+            }
+        }
+
+        return count + "";
+    }
+}
+
+class PickyGenerator extends Generator {
+    private final long multipleOf;
+
+    public PickyGenerator(long factor, long value, long multipleOf) {
+        super(factor, value);
+        this.multipleOf = multipleOf;
+    }
+
+    public void computeNext() {
+        do {
+            value = (value * factor) % 2147483647;
+        } while ((value % multipleOf) != 0);
+    }
+}
