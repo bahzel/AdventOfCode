e20609469808cbb8ccf12cee59a48826b421b6ec
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:48:43 +0200

    2017.9

---
+++ src/main/java/solution/y2017/Day9_2.java
@@ -0,0 +1,38 @@
+package solution.y2017;
+
+import utils.Tree;
+import utils.soution.Solution;
+
+public class Day9_2 extends Solution {
+    public static void main(String[] args) {
+        new Day9_2().solve();
+    }
+
+    @Override
+    protected String doSolve() {
+        String program = input.getFirst();
+        var parent = new Tree<>(0);
+        var state = ParserState.Group;
+        var garbageCount = 0;
+
+        for (int i = 0; i < program.length(); i++) {
+            if (state == ParserState.Group && '{' == program.charAt(i)) {
+                var child = new Tree<>(parent.getValue() + 1);
+                parent.addChild(child);
+                parent = child;
+            } else if (state == ParserState.Group && '}' == program.charAt(i)) {
+                parent = parent.getParent();
+            } else if (state == ParserState.Group && '<' == program.charAt(i)) {
+                state = ParserState.Garbage;
+            } else if (state == ParserState.Garbage && '>' == program.charAt(i)) {
+                state = ParserState.Group;
+            } else if (state == ParserState.Garbage && '!' == program.charAt(i)) {
+                i++;
+            } else if (state == ParserState.Garbage) {
+                garbageCount++;
+            }
+        }
+
+        return garbageCount + "";
+    }
+}
