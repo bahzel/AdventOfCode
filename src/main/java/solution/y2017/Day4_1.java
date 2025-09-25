e853822a881023d9db8ecdd440121222b0026b33
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:48:40 +0200

    2017.4

---
+++ src/main/java/solution/y2017/Day4_1.java
@@ -0,0 +1,36 @@
+package solution.y2017;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import utils.InstructionSolution;
+
+public class Day4_1 extends InstructionSolution<List<String>, AtomicInteger> {
+    public static void main(String[] args) {
+        new Day4_1().solve();
+    }
+
+    @Override
+    protected AtomicInteger initializeValue() {
+        return new AtomicInteger();
+    }
+
+    @Override
+    protected List<String> transformInstruction(String instruction) {
+        return List.of(instruction.split(" "));
+    }
+
+    @Override
+    protected boolean performInstruction(List<String> strings, AtomicInteger atomicInteger) {
+        if (new HashSet<>(strings).size() == strings.size()) {
+            atomicInteger.incrementAndGet();
+        }
+        return false;
+    }
+
+    @Override
+    protected String getSolution(AtomicInteger atomicInteger) {
+        return atomicInteger.toString();
+    }
+}
---
+++ src/main/java/solution/y2017/Day4_2.java
@@ -0,0 +1,42 @@
+package solution.y2017;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import utils.InstructionSolution;
+
+public class Day4_2 extends InstructionSolution<List<String>, AtomicInteger> {
+    public static void main(String[] args) {
+        new Day4_2().solve();
+    }
+
+    @Override
+    protected AtomicInteger initializeValue() {
+        return new AtomicInteger();
+    }
+
+    @Override
+    protected List<String> transformInstruction(String instruction) {
+        return List.of(instruction.split(" "));
+    }
+
+    @Override
+    protected boolean performInstruction(List<String> strings, AtomicInteger atomicInteger) {
+        if (strings.stream().map(this::sortString).distinct().count() == strings.size()) {
+            atomicInteger.incrementAndGet();
+        }
+        return false;
+    }
+
+    private String sortString(String input) {
+        char[] chars = input.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+
+    @Override
+    protected String getSolution(AtomicInteger atomicInteger) {
+        return atomicInteger.toString();
+    }
+}
