ad8e4d6dc1d77bbe3ac9225349886ae4aea8c34d
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:48:26 +0200

    2016.4

---
+++ src/main/java/solution/y2016/Day4_1.java
@@ -0,0 +1,78 @@
+package solution.y2016;
+
+import lombok.Getter;
+import utils.InstructionSolution;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+public class Day4_1 extends InstructionSolution<EncryptedRoom, AtomicLong> {
+	public static void main(String[] args) {
+		new Day4_1().solve();
+	}
+
+	@Override
+	protected AtomicLong initializeValue() {
+		return new AtomicLong();
+	}
+
+	@Override
+	protected EncryptedRoom transformInstruction(String instruction) {
+		return new EncryptedRoom(instruction);
+	}
+
+	@Override
+	protected boolean performInstruction(EncryptedRoom encryptedRoom, AtomicLong atomicLong) {
+		if (encryptedRoom.isValid()) {
+			atomicLong.addAndGet(encryptedRoom.getSectorId());
+		}
+
+		return false;
+	}
+
+	@Override
+	protected String getSolution(AtomicLong atomicLong) {
+		return atomicLong.toString();
+	}
+}
+
+class EncryptedRoom {
+	@Getter
+	private final String name;
+	private final String checkSum;
+	@Getter
+	private final long sectorId;
+
+	public EncryptedRoom(String input) {
+		name = input.substring(0, input.lastIndexOf('-'));
+		checkSum = input.substring(0, input.length() - 1).split("\\[")[1];
+		sectorId = Long.parseLong(input.substring(input.lastIndexOf('-') + 1, input.indexOf('[')));
+	}
+
+	public boolean isValid() {
+		return Objects.equals(computeChecksum(), checkSum);
+	}
+
+	private String computeChecksum() {
+		Map<Character, Integer> characterMap = new HashMap<>();
+
+		for (char c : name.replace("-", "").toCharArray()) {
+			if (characterMap.containsKey(c)) {
+				characterMap.put(c, characterMap.get(c) + 1);
+			} else {
+				characterMap.put(c, 1);
+			}
+		}
+
+		return characterMap.entrySet().stream().sorted((e1, e2) -> {
+			if (Objects.equals(e1.getValue(), e2.getValue())) {
+				return Character.compare(e1.getKey(), e2.getKey());
+			} else {
+				return Integer.compare(e2.getValue(), e1.getValue());
+			}
+		}).map(Map.Entry::getKey).map(Object::toString).collect(Collectors.joining()).substring(0, 5);
+	}
+}
---
+++ src/main/java/solution/y2016/Day4_2.java
@@ -0,0 +1,65 @@
+package solution.y2016;
+
+import utils.InstructionSolution;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Day4_2 extends InstructionSolution<DecryptableRoom, AtomicLong> {
+	public static void main(String[] args) {
+		new Day4_2().solve();
+	}
+
+	@Override
+	protected AtomicLong initializeValue() {
+		return new AtomicLong();
+	}
+
+	@Override
+	protected DecryptableRoom transformInstruction(String instruction) {
+		return new DecryptableRoom(instruction);
+	}
+
+	@Override
+	protected boolean performInstruction(DecryptableRoom decryptableRoom, AtomicLong atomicLong) {
+		if ("northpole object storage".equals(decryptableRoom.decrypt())) {
+			atomicLong.set(decryptableRoom.getSectorId());
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	protected String getSolution(AtomicLong atomicLong) {
+		return atomicLong.toString();
+	}
+}
+
+class DecryptableRoom extends EncryptedRoom {
+	public DecryptableRoom(String input) {
+		super(input);
+	}
+
+	public String decrypt() {
+		var solution = new StringBuilder();
+
+		for (char c : getName().toCharArray()) {
+			if (c == '-') {
+				solution.append(" ");
+			} else {
+				solution.append(rotate(c));
+			}
+		}
+		
+		return solution.toString();
+	}
+
+	private char rotate(char c) {
+		char rotated = c;
+		rotated += (char) (getSectorId() % 26);
+		if (rotated > 'z') {
+			rotated -= 26;
+		}
+		return rotated;
+	}
+}
