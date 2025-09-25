03ab998602829946bfb91e34eb59963275e5057d
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:50:03 +0200

    2019.3

---
+++ src/main/java/solution/y2019/Day3_1.java
@@ -0,0 +1,76 @@
+package solution.y2019;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.lang3.tuple.Pair;
+import utils.Direction;
+import utils.Point;
+import utils.soution.InstructionSolution;
+
+public class Day3_1 extends InstructionSolution<List<Pair<Integer, Direction>>, Set<Point>> {
+	public static void main(String[] args) {
+		new Day3_1().solve();
+	}
+
+	@Override
+	protected Set<Point> initializeValue() {
+		return new HashSet<>();
+	}
+
+	@Override
+	protected List<Pair<Integer, Direction>> transformInstruction(String instruction) {
+		var instructions = instruction.split(",");
+		var transformed = new ArrayList<Pair<Integer, Direction>>();
+
+		for (var direction : instructions) {
+			switch (direction.charAt(0)) {
+			case 'U' -> transformed.add(Pair.of(Integer.parseInt(direction.substring(1)), Direction.UP));
+			case 'D' -> transformed.add(Pair.of(Integer.parseInt(direction.substring(1)), Direction.DOWN));
+			case 'L' -> transformed.add(Pair.of(Integer.parseInt(direction.substring(1)), Direction.LEFT));
+			case 'R' -> transformed.add(Pair.of(Integer.parseInt(direction.substring(1)), Direction.RIGHT));
+			default -> throw new IllegalArgumentException("Invalid instruction: " + direction);
+			}
+		}
+
+		return transformed;
+	}
+
+	@Override
+	protected boolean performInstruction(List<Pair<Integer, Direction>> integerDirectionPair, Set<Point> points) {
+		if (points.isEmpty()) {
+			var currentPoint = new Point(0, 0);
+			for (var direction : integerDirectionPair) {
+				for (int i = 0; i < direction.getLeft(); i++) {
+					currentPoint = currentPoint.getNeighbour(direction.getRight());
+					points.add(currentPoint);
+				}
+			}
+		} else {
+			var crossings = new HashSet<Point>();
+			var currentPoint = new Point(0, 0);
+			for (var direction : integerDirectionPair) {
+				for (int i = 0; i < direction.getLeft(); i++) {
+					currentPoint = currentPoint.getNeighbour(direction.getRight());
+					if (points.contains(currentPoint)) {
+						crossings.add(currentPoint);
+					}
+				}
+			}
+			points.clear();
+			points.addAll(crossings);
+		}
+
+		return false;
+	}
+
+	@Override
+	protected String getSolution(Set<Point> points) {
+		var startingPoint = new Point(0, 0);
+
+		return points.stream().mapToInt(point -> point.computeManhattanDistance(startingPoint)).min().orElseThrow()
+				+ "";
+	}
+}
---
+++ src/main/java/solution/y2019/Day3_2.java
@@ -0,0 +1,78 @@
+package solution.y2019;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.tuple.Pair;
+import utils.Direction;
+import utils.Point;
+import utils.soution.InstructionSolution;
+
+public class Day3_2 extends InstructionSolution<List<Pair<Integer, Direction>>, Map<Point, Integer>> {
+	public static void main(String[] args) {
+		new Day3_2().solve();
+	}
+
+	@Override
+	protected Map<Point, Integer> initializeValue() {
+		return new HashMap<>();
+	}
+
+	@Override
+	protected List<Pair<Integer, Direction>> transformInstruction(String instruction) {
+		var instructions = instruction.split(",");
+		var transformed = new ArrayList<Pair<Integer, Direction>>();
+
+		for (var direction : instructions) {
+			switch (direction.charAt(0)) {
+			case 'U' -> transformed.add(Pair.of(Integer.parseInt(direction.substring(1)), Direction.UP));
+			case 'D' -> transformed.add(Pair.of(Integer.parseInt(direction.substring(1)), Direction.DOWN));
+			case 'L' -> transformed.add(Pair.of(Integer.parseInt(direction.substring(1)), Direction.LEFT));
+			case 'R' -> transformed.add(Pair.of(Integer.parseInt(direction.substring(1)), Direction.RIGHT));
+			default -> throw new IllegalArgumentException("Invalid instruction: " + direction);
+			}
+		}
+
+		return transformed;
+	}
+
+	@Override
+	protected boolean performInstruction(List<Pair<Integer, Direction>> integerDirectionPair,
+			Map<Point, Integer> points) {
+		if (points.isEmpty()) {
+			var currentPoint = new Point(0, 0);
+			var steps = 0;
+			for (var direction : integerDirectionPair) {
+				for (int i = 0; i < direction.getLeft(); i++) {
+					steps++;
+					currentPoint = currentPoint.getNeighbour(direction.getRight());
+					points.putIfAbsent(currentPoint, steps);
+				}
+			}
+		} else {
+			var crossings = new HashMap<Point, Integer>();
+			var currentPoint = new Point(0, 0);
+			var steps = 0;
+			for (var direction : integerDirectionPair) {
+				for (int i = 0; i < direction.getLeft(); i++) {
+					steps++;
+					currentPoint = currentPoint.getNeighbour(direction.getRight());
+					if (points.containsKey(currentPoint)) {
+						crossings.putIfAbsent(currentPoint, steps + points.get(currentPoint));
+					}
+				}
+			}
+			points.clear();
+			points.putAll(crossings);
+		}
+
+		return false;
+	}
+
+	@Override
+	protected String getSolution(Map<Point, Integer> points) {
+		return points.values().stream().mapToInt(Integer::intValue).min().orElseThrow() + "";
+	}
+}
