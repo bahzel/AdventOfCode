cd843d7e15d83cf3eb5308461b5ec619650b8ec1
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:48:58 +0200

    2018.4

---
+++ src/main/java/solution/y2018/Day4_2.java
@@ -0,0 +1,51 @@
+package solution.y2018;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.Pair;
+import utils.soution.Solution;
+
+public class Day4_2 extends Solution {
+	public static void main(String[] args) {
+		new Day4_2().solve();
+	}
+
+	@Override
+	protected void initialize() {
+		Collections.sort(input);
+	}
+
+	@Override
+	protected String doSolve() {
+		var guardList = getGuardList();
+		return guardList.stream()
+						.max(Comparator.comparingLong(guard -> guard.getMaximumSleepingMinute().getRight()))
+						.orElseThrow()
+						.getScore() + "";
+	}
+
+	private List<Guard> getGuardList() {
+		var guards = new HashMap<Long, Guard>();
+		Guard currentGuard = null;
+		var startTime = 0;
+
+		for (var instruction : input) {
+			var instructions = instruction.replace("] ", ":").split(":");
+			var time = Integer.parseInt(instructions[1]);
+			if (instructions[2].startsWith("Guard")) {
+				var id = Long.parseLong(instructions[2].split(" ")[1].substring(1));
+				currentGuard = guards.computeIfAbsent(id, Guard::new);
+			} else if (instructions[2].startsWith("falls")) {
+				startTime = time;
+			} else if (instructions[2].startsWith("wakes")) {
+				currentGuard.addAsleepTime(Pair.of(startTime, time));
+			}
+		}
+
+		return new ArrayList<>(guards.values());
+	}
+}
