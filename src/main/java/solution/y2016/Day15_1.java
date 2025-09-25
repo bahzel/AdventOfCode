9ffb42c0036acaa21db346890233f8a3cf9e81b0
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:48:31 +0200

    2016.15

---
+++ src/main/java/solution/y2016/Day15_1.java
@@ -0,0 +1,44 @@
+package solution.y2016;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.Pair;
+import utils.MapSolution;
+
+public class Day15_1 extends MapSolution<List<Pair<Integer, Integer>>> {
+    public static void main(String[] args) {
+        new Day15_1().solve();
+    }
+
+    @Override
+    protected List<Pair<Integer, Integer>> initializeMapping() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    protected void transformInstruction(String instruction, List<Pair<Integer, Integer>> pairs) {
+        var instructions = instruction.replace(".", "").split(" ");
+        pairs.add(Pair.of(Integer.parseInt(instructions[3]), Integer.parseInt(instructions[11])));
+    }
+
+    @Override
+    protected String computeSolution(List<Pair<Integer, Integer>> pairs) {
+        for (var i = 1L; i < Long.MAX_VALUE; i++) {
+            if (isValid(i, pairs)) {
+                return i - 1 + "";
+            }
+        }
+        throw new IllegalStateException("No solution found");
+    }
+
+    private boolean isValid(long time, List<Pair<Integer, Integer>> pairs) {
+        for (var j = 0; j < pairs.size(); j++) {
+            var pair = pairs.get(j);
+            if ((pair.getRight() + time + j) % pair.getLeft() != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
