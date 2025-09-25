7e2ba81927559c245ea90fe0233c993136c3dabe
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:50:15 +0200

    2024.19

---
+++ src/main/java/solution/y2024/Day19_1.java
@@ -0,0 +1,72 @@
+package solution.y2024;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import utils.soution.InstructionWithInputSolution;
+
+public class Day19_1 extends InstructionWithInputSolution<String, List<String>, AtomicInteger> {
+	public static void main(String[] args) {
+		new Day19_1().solve();
+	}
+
+	@Override
+	protected AtomicInteger initializeValue() {
+		return new AtomicInteger();
+	}
+
+	@Override
+	protected List<String> getInstructions(List<String> instructions) {
+		return instructions.subList(2, instructions.size());
+	}
+
+	@Override
+	protected String transformInstruction(String instruction) {
+		return instruction;
+	}
+
+	@Override
+	protected String getInput(List<String> instructions) {
+		return instructions.getFirst();
+	}
+
+	@Override
+	protected List<String> transformInput(String input) {
+		return Arrays.asList(input.split(", "));
+	}
+
+	@Override
+	protected void performInstruction(String instruction, List<String> towels, AtomicInteger solution) {
+		var cache = new HashSet<Integer>();
+
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(0);
+		while (!queue.isEmpty()) {
+			var currentIndex = queue.poll();
+			if (!cache.add(currentIndex)) {
+				continue;
+			}
+
+			if (currentIndex == instruction.length()) {
+				solution.incrementAndGet();
+				return;
+			}
+
+			var rest = instruction.substring(currentIndex);
+			for (var towel : towels) {
+				if (rest.startsWith(towel)) {
+					queue.add(currentIndex + towel.length());
+				}
+			}
+		}
+	}
+
+	@Override
+	protected String getSolution(AtomicInteger atomicInteger) {
+		return atomicInteger.toString();
+	}
+}
---
+++ src/main/java/solution/y2024/Day19_2.java
@@ -0,0 +1,73 @@
+package solution.y2024;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import utils.soution.InstructionWithInputSolution;
+
+public class Day19_2 extends InstructionWithInputSolution<String, List<String>, AtomicLong> {
+	private final Map<String, Long> cache = new HashMap<>();
+
+	public static void main(String[] args) {
+		new Day19_2().solve();
+	}
+
+	@Override
+	protected AtomicLong initializeValue() {
+		return new AtomicLong();
+	}
+
+	@Override
+	protected List<String> getInstructions(List<String> instructions) {
+		return instructions.subList(2, instructions.size());
+	}
+
+	@Override
+	protected String transformInstruction(String instruction) {
+		return instruction;
+	}
+
+	@Override
+	protected String getInput(List<String> instructions) {
+		return instructions.getFirst();
+	}
+
+	@Override
+	protected List<String> transformInput(String input) {
+		return Arrays.asList(input.split(", "));
+	}
+
+	@Override
+	protected void performInstruction(String instruction, List<String> towels, AtomicLong solution) {
+		solution.addAndGet(countPossibleCombinations(instruction, towels));
+	}
+
+	private long countPossibleCombinations(String instruction, List<String> towels) {
+		if (instruction.isEmpty()) {
+			return 1;
+		}
+
+		var cachedValue = cache.get(instruction);
+		if (cachedValue != null) {
+			return cachedValue;
+		}
+
+		var combinationCount = 0L;
+		for (var towel : towels) {
+			if (instruction.startsWith(towel)) {
+				combinationCount += countPossibleCombinations(instruction.substring(towel.length()), towels);
+			}
+		}
+
+		cache.put(instruction, combinationCount);
+		return combinationCount;
+	}
+
+	@Override
+	protected String getSolution(AtomicLong solution) {
+		return solution.toString();
+	}
+}
