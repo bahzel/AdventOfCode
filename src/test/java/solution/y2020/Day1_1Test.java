9c6da2d1fec8bca0cf9cdb2134948c68b6bd175c
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:50:49 +0200

    enable tests

--- src/test/java/solution/y2020/Day1_1Test.java
+++ src/test/java/solution/y2020/Day1_1Test.java
@@ -1,11 +1,9 @@
 package solution.y2020;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Disabled
 public class Day1_1Test {
 	@Test
 	public void test() {
