70bdf5dab63c1b043f87931be47930595f12f768
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:48:29 +0200

    2016.11

---
+++ src/test/java/solution/y2016/Day11_2Test.java
@@ -0,0 +1,12 @@
+package solution.y2016;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Day11_2Test {
+    @Test
+    public void test() {
+        assertThat(new Day11_2().solve()).isEqualTo("55");
+    }
+}
