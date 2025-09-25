177f92160cdfaf22b71b53113b0e107e445c5b20
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:48:38 +0200

    2017.1

---
+++ src/main/java/solution/y2017/Day1_2.java
@@ -0,0 +1,24 @@
+package solution.y2017;
+
+import utils.Solution;
+
+public class Day1_2 extends Solution {
+    public static void main(String[] args) {
+        new Day1_2().solve();
+    }
+
+    @Override
+    protected String doSolve() {
+        var sum = 0L;
+        var captcha = input.getFirst();
+        var halfSize = captcha.length() / 2;
+
+        for (int i = 0; i < halfSize; i++) {
+            if (captcha.charAt(i) == captcha.charAt(i + halfSize)) {
+                sum += Long.parseLong(captcha.charAt(i) + "") * 2;
+            }
+        }
+
+        return sum + "";
+    }
+}
---
+++ src/test/java/solution/y2017/Day1_1Test.java
@@ -0,0 +1,12 @@
+package solution.y2017;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Day1_1Test {
+    @Test
+    public void test() {
+        assertThat(new Day1_1().solve()).isEqualTo("1069");
+    }
+}
