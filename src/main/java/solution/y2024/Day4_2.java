f48615328031b6f04d4f16139e8bc702d698a8eb
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:49:40 +0200

    2024.4

---
+++ src/main/java/solution/y2024/Day4_2.java
@@ -0,0 +1,69 @@
+package solution.y2024;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import utils.Point;
+import utils.soution.Solution;
+
+public class Day4_2 extends Solution {
+	public static void main(String[] args) {
+		new Day4_2().solve();
+	}
+
+	@Override
+	protected String doSolve() {
+		var wordSearch = getWordSearch();
+		var directions = getDirections();
+		var counter = 0;
+
+		for (var x = 0; x < wordSearch.length; x++) {
+			for (var y = 0; y < wordSearch[x].length; y++) {
+				for (var direction : directions) {
+					if (getCharAt(wordSearch, x, y, direction.getFirst()) == 'M'
+							&& getCharAt(wordSearch, x, y, direction.get(1)) == 'M'
+							&& getCharAt(wordSearch, x, y, direction.get(2)) == 'A'
+							&& getCharAt(wordSearch, x, y, direction.get(3)) == 'S'
+							&& getCharAt(wordSearch, x, y, direction.get(4)) == 'S') {
+						counter++;
+					}
+				}
+			}
+		}
+
+		return counter + "";
+	}
+
+	private char getCharAt(char[][] wordSearch, int x, int y, Point point) {
+		int adjustedX = x + point.getX();
+		int adjustedY = y + point.getY();
+
+		if (adjustedX < 0 || adjustedX >= wordSearch.length || adjustedY < 0
+				|| adjustedY >= wordSearch[adjustedX].length) {
+			return 0;
+		} else {
+			return wordSearch[adjustedX][adjustedY];
+		}
+	}
+
+	private char[][] getWordSearch() {
+		char[][] wordSearch = new char[input.getFirst().length()][input.size()];
+		for (int y = 0; y < input.getFirst().length(); y++) {
+			for (int x = 0; x < input.size(); x++) {
+				wordSearch[x][y] = input.get(y).charAt(x);
+			}
+		}
+		return wordSearch;
+	}
+
+	private List<List<Point>> getDirections() {
+		var directions = new ArrayList<List<Point>>();
+
+		directions.add(List.of(new Point(0, 0), new Point(0, 2), new Point(1, 1), new Point(2, 0), new Point(2, 2)));
+		directions.add(List.of(new Point(0, 0), new Point(2, 0), new Point(1, 1), new Point(0, 2), new Point(2, 2)));
+		directions.add(List.of(new Point(2, 0), new Point(2, 2), new Point(1, 1), new Point(0, 2), new Point(0, 0)));
+		directions.add(List.of(new Point(2, 2), new Point(0, 2), new Point(1, 1), new Point(0, 0), new Point(2, 0)));
+
+		return directions;
+	}
+}
