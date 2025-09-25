1cd62b125675e46131371cd66899c8ae8b3263e5
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:48:26 +0200

    2016.6

---
+++ src/main/java/solution/y2016/Day6_1.java
@@ -0,0 +1,39 @@
+package solution.y2016;
+
+import utils.Solution;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day6_1 extends Solution {
+	public static void main(String[] args) {
+		new Day6_1().solve();
+	}
+
+	@Override
+	protected String doSolve() {
+		var solution = new StringBuilder();
+
+		for (int i = 0; i < input.getFirst().length(); i++) {
+			Map<Character, Integer> characterMap = new HashMap<>();
+
+			for (var message : input) {
+				var c = message.charAt(i);
+				if (characterMap.containsKey(c)) {
+					characterMap.put(c, characterMap.get(c) + 1);
+				} else {
+					characterMap.put(c, 1);
+				}
+			}
+
+			solution.append(characterMap.entrySet()
+										.stream()
+										.max(Comparator.comparingInt(Map.Entry::getValue))
+										.orElseThrow()
+										.getKey());
+		}
+
+		return solution.toString();
+	}
+}
