e17a5d60f8ab37b7c46a2ace7fcf377616a1c595
UH2WYMC <daniel.wagner@vwgis.de>
2025-09-25 18:48:34 +0200

    2016.19

---
+++ src/main/java/solution/y2016/Day19_1.java
@@ -0,0 +1,34 @@
+package solution.y2016;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import utils.Solution;
+
+public class Day19_1 extends Solution {
+    public static void main(String[] args) {
+        new Day19_1().solve();
+    }
+
+    @Override
+    protected String doSolve() {
+        var elfCount = Integer.parseInt(input.getFirst());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= elfCount; i++) {
+            queue.add(i);
+        }
+
+        while (!queue.isEmpty()) {
+            int elf = queue.poll();
+
+            if (queue.isEmpty()) {
+                return elf + "";
+            }
+
+            queue.poll();
+            queue.add(elf);
+        }
+        throw new IllegalStateException();
+    }
+}
---
+++ src/main/java/solution/y2016/Day19_2.java
@@ -0,0 +1,55 @@
+package solution.y2016;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+import utils.Solution;
+
+public class Day19_2 extends Solution {
+    public static void main(String[] args) {
+        new Day19_2().solve();
+    }
+
+    @Override
+    protected String doSolve() {
+        var elfCount = Integer.parseInt(input.getFirst());
+        var queue = new LinkedList<Integer>();
+
+        for (int i = 1; i <= elfCount; i++) {
+            queue.add(i);
+        }
+
+        var iterator = new CyclingIntIterator(queue);
+        while (queue.size() > 1) {
+            iterator.next();
+            iterator.remove();
+            iterator.next();
+            iterator.remove();
+            iterator.next();
+        }
+        return iterator.next() + "";
+    }
+}
+
+class CyclingIntIterator {
+    private final LinkedList<Integer> list;
+    private ListIterator<Integer> iterator;
+
+    public CyclingIntIterator(LinkedList<Integer> list) {
+        this.list = list;
+        iterator = list.listIterator(list.size() / 2);
+    }
+
+    public int next() {
+        if (!iterator.hasNext()) {
+            iterator = list.listIterator();
+        }
+        return iterator.next();
+    }
+
+    public void remove() {
+        if (list.size() > 1) {
+            iterator.remove();
+        }
+    }
+}
