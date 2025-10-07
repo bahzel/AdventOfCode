7c1dfea592a179d90bc2c571912f3ee99bddfed3
UH2WYMC <daniel.wagner@vwgis.de>
2025-10-07 20:04:31 +0200

    2022.17_1

---
+++ src/test/java/solution/y2022/Day17_1Test.java
@@ -0,0 +1,12 @@
+package solution.y2022;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Day17_1Test {
+	@Test
+	public void test() {
+		assertThat(new Day17_1().disableLog().solve()).isEqualTo("3209");
+	}
+}
