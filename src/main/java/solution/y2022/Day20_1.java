58f75cfaa8309cceba84a9a705d3f6aa58ab1fbd
UH2WYMC <daniel.wagner@vwgis.de>
2025-10-11 16:43:36 +0200

    2022.20_1

---
+++ src/main/java/solution/y2022/Day20_1.java
@@ -0,0 +1,50 @@
+package solution.y2022;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import utils.soution.MapSolution;
+
+public class Day20_1 extends MapSolution<List<AtomicLong>> {
+	public static void main(String[] args) {
+		new Day20_1().solve();
+	}
+
+	@Override
+	protected List<AtomicLong> initializeMapping() {
+		return new ArrayList<>();
+	}
+
+	@Override
+	protected void transformInstruction(String instruction, List<AtomicLong> longList) {
+		longList.add(new AtomicLong(Long.parseLong(instruction)));
+	}
+
+	@Override
+	protected String computeSolution(List<AtomicLong> longList) {
+		var workingList = new LinkedList<>(longList);
+		for (AtomicLong currentValue : longList) {
+			var currentIndex = workingList.indexOf(currentValue);
+			workingList.remove(currentIndex);
+			var newIndex = (currentIndex + currentValue.get())
+					- (currentIndex + currentValue.get()) / workingList.size() * workingList.size();
+			if (newIndex <= 0) {
+				newIndex += workingList.size();
+			}
+			workingList.add((int) newIndex, currentValue);
+		}
+
+		var zeroIndex = 0;
+		for (int i = 0; i < workingList.size(); i++) {
+			if (workingList.get(i).get() == 0L) {
+				zeroIndex = i;
+				break;
+			}
+		}
+		return workingList.get((zeroIndex + 1000) % workingList.size()).get()
+				+ workingList.get((zeroIndex + 2000) % workingList.size()).get()
+				+ workingList.get((zeroIndex + 3000) % workingList.size()).get() + "";
+	}
+}
---
+++ src/test/java/solution/y2022/Day20_1Test.java
@@ -0,0 +1,12 @@
+package solution.y2022;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Day20_1Test {
+	@Test
+	public void test() {
+		assertThat(new Day20_1().disableLog().solve()).isEqualTo("7225");
+	}
+}
